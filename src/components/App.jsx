db072fff2ce551f54072251a16d77204459ad233
Devi Amaolo <deviamaolo@gmail.com>
2022-04-04 20:30:09 -0300

    creating login component and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import '../styles/global.scss';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+import '../styles/global.css';
 
 export const App = () => {
   return (
-    <h1>React shop</h1>
+    <Layout>
+      <Login />
+    </Layout>
   )
 }
 
