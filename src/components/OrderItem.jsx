5d23ff6fb3e8ab424292223cc170e89f2a221f56
Devi Amaolo <deviamaolo@gmail.com>
2022-04-06 19:02:54 -0300

    creating hooks and adding alias in webpack

--- src/components/OrderItem.jsx
+++ src/components/OrderItem.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
-import '../styles/ProductItem.scss';
+import '@styles/OrderItem.scss';
+import close from '@icons/icon_close.png'
 
-const ProductItem = () => {
+const OrderItem = () => {
 	return (
-		<div className="ProductItem">
-			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
-			<div className="product-info">
-				<div>
-					<p>$120,00</p>
-					<p>Bike</p>
-				</div>
-				<figure>
-					<img src="./icons/bt_add_to_cart.svg" alt="" />
-				</figure>
-			</div>
+		<div className="OrderItem">
+			<figure>
+				<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="bike" />
+			</figure>
+			<p>Bike</p>
+			<p>$30,00</p>
+			<img src={close} alt="close" />
 		</div>
 	);
 }
 
-export default ProductItem;
+export default OrderItem;
